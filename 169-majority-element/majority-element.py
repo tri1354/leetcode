c709f2334bd945f16a1dd70a79d4c5ea8f6d2267
tri1354 <117267881+tri1354@users.noreply.github.com>
2026-03-17 23:05:08 +0700

    Time: 7 ms (57.60%) | Memory: 21.1 MB (78.82%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,7 +1,17 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        d = {}
+        # d = {}
+        # for num in nums:
+        #     d[num] = d.get(num, 0) + 1
+        # return max(d, key=d.get)
+
+        # Boyer–Moore
+        ans = 0
+        count = 0
+
         for num in nums:
-            d[num] = d.get(num, 0) + 1
+            if count == 0:
+                ans = num
+            count += (1 if num == ans else -1)
         
-        return max(d, key=d.get)
+        return ans
