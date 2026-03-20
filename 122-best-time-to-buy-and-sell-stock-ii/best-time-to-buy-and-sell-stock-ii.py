b4123763f75aadf10e0c88e46a80c1c63141cdac
tri1354 <117267881+tri1354@users.noreply.github.com>
2026-03-20 20:36:05 +0700

    Time: 0 ms (100.00%) | Memory: 20.4 MB (71.53%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,19 +1,26 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        prices.insert(0, prices[0])
-        prices.append(prices[-1])
+        # prices.insert(0, prices[0])
+        # prices.append(prices[-1])
 
-        min_price = float('inf')
-        profit = 0
+        # min_price = float('inf')
+        # profit = 0
+        # max_profit = 0
+
+        # for i in range(1, len(prices)-1):
+        #     if prices[i] < min_price:
+        #         min_price = prices[i]
+        #     profit = max(profit, prices[i] - min_price)
+
+        #     if prices[i] <= prices[i-1] and prices[i] < prices[i+1] and i != len(prices)-1:
+        #         max_profit -= prices[i]
+        #     if prices[i] > prices[i-1] and prices[i] >= prices[i+1] and i != 1:
+        #         max_profit += prices[i]
+        # return max(profit, max_profit)
+
+
         max_profit = 0
-
-        for i in range(1, len(prices)-1):
-            if prices[i] < min_price:
-                min_price = prices[i]
-            profit = max(profit, prices[i] - min_price)
-
-            if prices[i] <= prices[i-1] and prices[i] < prices[i+1] and i != len(prices)-1:
-                max_profit -= prices[i]
-            if prices[i] > prices[i-1] and prices[i] >= prices[i+1] and i != 1:
-                max_profit += prices[i]
-        return max(profit, max_profit)
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                max_profit += prices[i] - prices[i-1]
+        return max_profit
