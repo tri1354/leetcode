dc454135e40832d65d6c75ca6cf3ddf38bab773a
tri1354 <117267881+tri1354@users.noreply.github.com>
2026-03-21 19:59:38 +0700

    Time: 13 ms (83.08%) | Memory: 20.7 MB (15.97%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,12 +1,15 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        checkpoint = 0
-        checkpoint_pos = len(nums)-1
-        for i in range(len(nums)-1, -1, -1):
-            if nums[i] >= checkpoint_pos - i:
-                checkpoint_pos = i
-                checkpoint = nums[i]
-        if checkpoint_pos == 0:
-            return True
-        else:
-            return False
+        # checkpoint_pos = len(nums)-1
+        # for i in range(len(nums)-1, -1, -1):
+        #     if nums[i] >= checkpoint_pos - i:
+        #         checkpoint_pos = i
+        # return checkpoint_pos == 0
+
+
+        max_reach = 0
+        for i in range(len(nums)):
+            if i > max_reach:
+                return False
+            max_reach = max(max_reach, nums[i] + i)
+        return True
