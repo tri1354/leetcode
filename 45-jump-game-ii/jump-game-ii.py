162c0245dcf64df1c57e02483e56844e4d610bfa
tri1354 <117267881+tri1354@users.noreply.github.com>
2026-03-22 17:05:15 +0700

    Time: 3 ms (88.64%) | Memory: 20 MB (83.87%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.py
@@ -0,0 +1,37 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        steps = 0
+        checkpoint_pos = 0
+        checkpoint = nums[0]
+        if checkpoint_pos == len(nums)-1:
+            return 0
+        while checkpoint < len(nums)-1:
+            temp_pos = checkpoint_pos
+            for i in range(checkpoint_pos, checkpoint+1):
+                if nums[i]+i > checkpoint:
+                    checkpoint = nums[i]+i
+                    temp_pos = i
+            checkpoint_pos = temp_pos
+            steps += 1
+        return steps + 1
+
+
+        # steps = 0
+        # checkpoint_pos = 0
+        # checkpoint = 0
+        # if checkpoint_pos == len(nums)-1:
+        #     return 0
+        # for i in range(len(nums)):
+        #     if nums[i]+i >= len(nums)-1:
+        #         steps += 1
+        #         break
+        #     else:
+        #         if nums[i]+i > checkpoint+checkpoint_pos:
+        #             checkpoint_pos = i
+        #             checkpoint = nums[i]
+        #             steps += 1
+        #         if i+checkpoint < len(nums):
+        #             if checkpoint_pos+checkpoint + nums[checkpoint_pos+checkpoint] >= len(nums)-1:
+        #                 steps += 1
+        #                 break 
+        # return steps
