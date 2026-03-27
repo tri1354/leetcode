853bbd511e32b4dc592bea2320ca7f652f983035
tri1354 <117267881+tri1354@users.noreply.github.com>
2026-03-27 21:42:00 +0700

    Time: 3 ms (78.18%) | Memory: 19.2 MB (95.76%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,25 +1,35 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        roman = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
-        integer = []
-        N = num
-        for i in range(0, 7, 2):
-            n = N % 10
-            if n == 0:
-                pass
-            elif n == 5:
-                integer.insert(0, roman[i+1])
-            elif n == 9:
-                integer.insert(0, roman[i+2])
-                integer.insert(0, roman[i])
-            elif n == 4:
-                integer.insert(0, roman[i+1])
-                integer.insert(0, roman[i])
-            else:
-                for j in range(n%5):
-                    integer.insert(0, roman[i])
-                if n//5 == 1:
-                    integer.insert(0, roman[i+1])
-            N //= 10
-        ans = "".join(integer)
+        romans = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+        values = [1000, 900, 500, 400,  100,  90,  50,   40,  10,   9,    5,   4,    1]
+        ans = ""
+        for i in range(len(values)):
+            while num >= values[i]:
+                ans += romans[i]
+                num -= values[i]
         return ans
+
+
+        # roman = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+        # integer = []
+        # N = num
+        # for i in range(0, 7, 2):
+        #     n = N % 10
+        #     if n == 0:
+        #         pass
+        #     elif n == 5:
+        #         integer.insert(0, roman[i+1])
+        #     elif n == 9:
+        #         integer.insert(0, roman[i+2])
+        #         integer.insert(0, roman[i])
+        #     elif n == 4:
+        #         integer.insert(0, roman[i+1])
+        #         integer.insert(0, roman[i])
+        #     else:
+        #         for j in range(n%5):
+        #             integer.insert(0, roman[i])
+        #         if n//5 == 1:
+        #             integer.insert(0, roman[i+1])
+        #     N //= 10
+        # ans = "".join(integer)
+        # return ans
