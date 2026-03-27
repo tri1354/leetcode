93f97f168aba0f4a7d2841cbcc68020950ce3b7c
tri1354 <117267881+tri1354@users.noreply.github.com>
2026-03-27 20:01:09 +0700

    Time: 7 ms (74.75%) | Memory: 21.1 MB (67.70%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,17 +1,34 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        n = len(height)
-        max_heights = [0] * n
-        max_height = 0
-        for i in range(n):
-            max_heights[i] = max_height
-            max_height = max(max_height, height[i])
-        max_height = 0
+        left = 0
+        right = len(height) - 1
+        max_left = height[left]
+        max_right = height[right]
         water = 0
-        for i in range(n-1, -1, -1):
-            water += max(0, min(max_height, max_heights[i]) - height[i])
-            max_height = max(max_height, height[i])
+        while left < right:
+            if max_left < max_right:
+                left += 1
+                max_left = max(max_left, height[left])
+                water += max_left - height[left]
+            else:
+                right -= 1
+                max_right = max(max_right, height[right])
+                water += max_right - height[right]
         return water
+
+
+        # n = len(height)
+        # max_heights = [0] * n
+        # max_height = 0
+        # for i in range(n):
+        #     max_heights[i] = max_height
+        #     max_height = max(max_height, height[i])
+        # max_height = 0
+        # water = 0
+        # for i in range(n-1, -1, -1):
+        #     water += max(0, min(max_height, max_heights[i]) - height[i])
+        #     max_height = max(max_height, height[i])
+        # return water
 
 
         # n = len(height)
