fa7746b27d464f0427fc49c36a8eb3217f04a621
tri1354 <117267881+tri1354@users.noreply.github.com>
2026-04-02 20:21:15 +0700

    Time: 9 ms (42.42%) | Memory: 20.1 MB (33.78%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,10 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         result = "".join(c for c in s if c.isalnum()).lower()
-        n = len(result) - 1
-        if result == "":
-            return True
-        for i in range(len(result)//2):
-            if result[i] != result[n-i]:
-                return False
-        return True
+        return result == result[::-1]
+
+
+        # n = len(result) - 1
+        # if result == "":
+        #     return True
+        # for i in range(len(result)//2):
+        #     if result[i] != result[n-i]:
+        #         return False
+        # return True
