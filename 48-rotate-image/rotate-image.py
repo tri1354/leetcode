78bd37dc71a5faabcefc3f4eae142cce6fc0d787
tri1354 <117267881+tri1354@users.noreply.github.com>
2026-04-14 21:08:26 +0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (73.89%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -3,13 +3,22 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        first = 0
-        last = len(matrix) - 1
-        n = last
-        while first < last:
-            for i in range(first, last):
-                matrix[first][i], matrix[i][last] = matrix[i][last], matrix[first][i]
-                matrix[first][i], matrix[n - i][first] = matrix[n - i][first], matrix[first][i]
-                matrix[n - i][first], matrix[last][n - i] = matrix[last][n - i], matrix[n - i][first]
-            first += 1
-            last -= 1
+        row = len(matrix)
+        col = len(matrix[0])
+        for i in range(row):
+            for j in range(i + 1, col):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for k in range(row):
+            matrix[k].reverse()
+
+
+        # first = 0
+        # last = len(matrix) - 1
+        # n = last
+        # while first < last:
+        #     for i in range(first, last):
+        #         matrix[first][i], matrix[i][last] = matrix[i][last], matrix[first][i]
+        #         matrix[first][i], matrix[n - i][first] = matrix[n - i][first], matrix[first][i]
+        #         matrix[n - i][first], matrix[last][n - i] = matrix[last][n - i], matrix[n - i][first]
+        #     first += 1
+        #     last -= 1
